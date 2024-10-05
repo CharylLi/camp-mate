63bc92f1a5fa0b059f6b207469196d2cf65c6eaf
mengyuli <mengyuli@cs.washington.edu>
2024-10-05 12:24:05 -0700

    Initial commit

---
+++ controllers/campgrounds.js
@@ -0,0 +1,105 @@
+const Campground = require('../models/campground');
+const { cloudinary } = require("../cloudinary");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/new');
+}
+
+module.exports.createCampground = async (req, res, next) => {
+    try {
+        // Use the location input from the form
+        const location = req.body.campground.location;
+
+        // Make a request to the Mapbox Geocoding API
+        const geoData = await geocoder.forwardGeocode({
+            query: location,  // Pass the location from the form
+            limit: 1  // We only want the first result
+        }).send();
+
+        // Check if Mapbox returns valid geometry
+        if (!geoData.body.features.length) {
+            req.flash('error', 'Invalid location. Please provide a valid location.');
+            return res.redirect('back');
+        }
+
+        // Log the response from Mapbox to see what is being returned
+        console.log('Geocoding data:', geoData.body.features[0].geometry);
+
+        // Create a new campground with the form data and set the geometry field from Mapbox
+        const campground = new Campground(req.body.campground);
+        campground.geometry = geoData.body.features[0].geometry;
+
+        // Add images and author
+        campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+        campground.author = req.user._id;
+
+        // Save the campground to the database
+        await campground.save();
+
+        // Redirect and show success message
+        req.flash('success', 'Successfully created a new campground!');
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (error) {
+        // Handle errors
+        console.error('Error creating campground:', error);
+        req.flash('error', 'Something went wrong, please try again.');
+        res.redirect('back');
+    }
+};
+
+module.exports.showCampground = async (req, res,) => {
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}
+
+module.exports.updateCampground = async (req, res) => {
+    const { id } = req.params;
+    console.log(req.body);
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
+    req.flash('success', 'Successfully updated campground!');
+    res.redirect(`/campgrounds/${campground._id}`)
+};
+
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground')
+    res.redirect('/campgrounds');
+}
---
+++ public/javascripts/validateForms.js
@@ -0,0 +1,20 @@
+// const { default: bsCustomFileInput } = require("bs-custom-file-input");
+
+(function () {
+    'use strict';
+    // bsCustomFileInput.init()
+
+    window.addEventListener('load', function () {
+        const forms = document.getElementsByClassName('validated-form');
+        const validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+
+})()
